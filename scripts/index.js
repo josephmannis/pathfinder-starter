46c8caf1939ddf9e7c6afdb4b18a5e1dcbb2dfb3
josephmannis <annis.jo@husky.neu.edu>
2020-11-04 17:24:24 -0500

    Remove key.

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-function getWeather() {
-    fetch('api.openweathermap.org/data/2.5/weather?q=Boston&appid=0c43b8857083042c87517c926a41ff4d')
-    .then(res => console.log(JSON.parse(res)))
-}
-
-getWeather()
